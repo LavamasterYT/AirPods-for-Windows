96db5e7f9ba2ebc9cfcf832f7d260093d56afd8f
LavamasterYT <48543854+LavamasterYT@users.noreply.github.com>
2020-09-01 19:57:14 -0500

    idk what i did

---
+++ AirpodsUI/Service/ServiceContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Service
+{
+    class ServiceContext : ApplicationContext
+    {
+        private NotifyIcon TrayIcon;
+
+        public ServiceContext()
+        {
+
+        }
+    }
+}
